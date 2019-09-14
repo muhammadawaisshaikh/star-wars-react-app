45d81de0825824c4a279b0c57c5deb3d11f5791d
Muhammad Awais <awaisshaikh94@gmail.com>
2019-09-14 17:39:25 +0500

    Film page should show all the information pulled from the SWAPI endpoint for an individual film

--- src/components/details/details.jsx
+++ src/components/details/details.jsx
@@ -8,7 +8,8 @@ export default class Details extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [],
+        loading: 0,
+        data: [],
     };
   }
 
@@ -19,22 +20,80 @@ export default class Details extends React.Component {
     console.log(film);
 
     this.setState({
-      data: film
+        loading: 1,
+        data: film
     });
   });
 
 }
 
   render() {
-    const { data } = this.state.data;
-    return (
-        <React.Fragment>
+    const data = this.state.data;
+    if (this.state.loading === 1) {
+        return (
+            <React.Fragment>
+    
+            <div>
+                <h4>Film Name: {data.title} <span className="float-right">{data.director}</span></h4>
+                <p className="py-2">Release Date: {data.release_date}</p>
+                <hr/>
+                <p>Description: {data.opening_crawl}</p>
+            </div>
+            
+            <hr/>
+            <h4 className="py-2">Planets</h4>
+            {
+                data.planets.map((value, index) => {
+                return <div className="project" key={index}>
+                            <p>{value}</p>
+                        </div>
+                })
+            }
 
-        <div>
+            <hr/>
+            <h4 className="py-2">Species</h4>
+            {
+                data.species.map((value, index) => {
+                return <div className="project" key={index}>
+                            <p>{value}</p>
+                        </div>
+                })
+            }
 
-        </div>
+            <hr/>
+            <h4 className="py-2">Starships</h4>
+            {
+                data.starships.map((value, index) => {
+                return <div className="project" key={index}>
+                            <p>{value}</p>
+                        </div>
+                })
+            }
 
-        </React.Fragment>
-      );
+            <hr/>
+            <h4 className="py-2">Vehicles</h4>
+            {
+                data.vehicles.map((value, index) => {
+                return <div className="project" key={index}>
+                            <p>{value}</p>
+                        </div>
+                })
+            }
+
+            <hr/>
+    
+            </React.Fragment>
+          );
+    } else {
+        return (
+            <React.Fragment>
+    
+            <div>
+                <p>Loading ...</p>
+            </div>
+    
+            </React.Fragment>
+          );
+    }
   }
 }
